4ae4a9807522e86ae22f7c696898d1aa4673de55
sorude26 <sorude26@gmail.com>
2022-04-07 19:39:56 +0900

    プレイヤーの入力を受け取るスクリプトを追加した

---
+++ Assets/Scripts/PlayerInputManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputType
+{
+    Up,
+    Down,
+    Left,
+    Right,
+    Move,
+    Jump,
+    Fire1,
+    Fire2,
+    Fire3,
+    Submit,
+}
+public class PlayerInputManager : MonoBehaviour
+{
+    public static event Action<InputType> OnEnterInput = default;
+    public static event Action<InputType> OnStayInput = default;
+    public static event Action<InputType> OnExitInput = default;
+    public static Vector2 InputVector { get; private set; }
+    private void Update()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        InputVector = Vector2.right * h;
+        OnStayInput?.Invoke(InputType.Move);
+    }
+}
