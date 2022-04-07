3a69636f3dd05ea011f02a2048dd2b40bf7b2e09
sorude26 <sorude26@gmail.com>
2022-04-07 19:40:11 +0900

    移動操作用スクリプトを追加した

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class MoveController : MonoBehaviour
+{
+    public const float GRAVITY_POWER = -9.8f;
+
+    [SerializeField]
+    protected float _maxFallSpeed = 20f;
+    [SerializeField]
+    protected float _moveSpeed = 2f;
+    [SerializeField]
+    protected float _runSpeed = 6f;
+    [SerializeField]
+    protected float _jumpSpeed = 10f;
+    [SerializeField]
+    protected float _gravityScale = 1f;
+
+    private Rigidbody2D _rigidbody = default;
+    Vector2 _moveVector = default;
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        AddGravity();
+        _rigidbody.velocity = _moveVector;
+    }
+    private void AddGravity()
+    {
+        _moveVector.y = _gravityScale * GRAVITY_POWER;
+    }
+    public void Move(Vector2 dir)
+    {
+        _moveVector.x = _moveSpeed * dir.x;
+    }
+    public void Jump()
+    {
+        _moveVector.y = _jumpSpeed;
+    }
+}
