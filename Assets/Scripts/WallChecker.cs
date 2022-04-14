d8659b8e3f5f8967d0efee7beb96b3b093db4288
sorude26 <sorude26@gmail.com>
2022-04-14 15:30:37 +0900

    コメントの追加を行った

--- Assets/Scripts/WallChecker.cs
+++ Assets/Scripts/WallChecker.cs
@@ -5,27 +5,39 @@ using UnityEngine;
 public class WallChecker : MonoBehaviour
 {
     #region SerializeField
+    /// <summary> Linecastの始点 </summary>
     [SerializeField]
     private Transform[] _checkPoints = default;
+    /// <summary> Linecastの方向 </summary>
     [SerializeField]
     private Vector2 _checkDir = Vector2.down;
+    /// <summary> Linecastの長さ </summary>
     [SerializeField]
     private float _checkRange = 0.2f;
+    /// <summary> 壁のレイヤー </summary>
     [SerializeField]
-    private LayerMask _layer = default;
+    private LayerMask _wallLayer = default;
+    /// <summary> 最低接地判定数 </summary>
+    [SerializeField]
+    private int _minHitCount = 1;
     #endregion
-    public bool IsWalled(int maxHitCount = 1) 
+
+    /// <summary>
+    /// 壁判定があればtrueを返す
+    /// </summary>
+    /// <returns></returns>
+    public bool IsWalled()
     {
         int count = 0;
         foreach (Transform point in _checkPoints)
         {
             Vector2 start = point.position;
-            Vector2 end = start + _checkDir * _checkRange;
-            bool isHit = Physics2D.Linecast(start, end, _layer);
+            Vector2 end = start + _checkDir.normalized * _checkRange;
+            bool isHit = Physics2D.Linecast(start, end, _wallLayer);
             if (isHit)
             {
                 count++;
-                if (count >= maxHitCount)
+                if (count >= _minHitCount)
                 {
                     return true;
                 }
