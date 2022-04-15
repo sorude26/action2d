b817f481e622c459a4455cd3353a7e82cab1adc5
sorude26 <sorude26@gmail.com>
2022-04-15 23:56:09 +0900

    コメントの追加を行った

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -2,15 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ì¡íËÇÃÉIÉuÉWÉFÉNÉgÇ…í«è]Ç∑ÇÈÉNÉâÉX
+/// </summary>
 public class FollowObject : MonoBehaviour
 {
     #region SerializeField
+    /// <summary> í«è]ëŒè€ </summary>
     [SerializeField]
     Transform _followTarget = default;
+    /// <summary> í«è]ë¨ìx </summary>
     [SerializeField]
     float _followSpeed = 1f;
+    /// <summary> í«è]ï˚å¸ëŒè€ </summary>
     [SerializeField]
     Transform _rotationTarget = default;
+    /// <summary> âÒì]ë¨ìx </summary>
     [SerializeField]
     float _rotationSpeed = 1f;
     #endregion
@@ -32,21 +39,32 @@ public class FollowObject : MonoBehaviour
         if (!_isFollow) { return; }
         FollowMove();
     }
+    /// <summary>
+    /// í«è]à⁄ìÆ
+    /// </summary>
     private void FollowMove()
     {
         _followPos = _followTarget.position;
         float speed = (transform.position - _followPos).sqrMagnitude;
         transform.position = Vector3.Lerp(transform.position, _followPos, speed * _followSpeed * Time.deltaTime);
     }
+    /// <summary>
+    /// í«â¡âÒì]
+    /// </summary>
     private void FollowLook()
     {
         transform.forward = Vector3.Lerp(transform.forward, _rotationTarget.forward, _rotationSpeed * Time.deltaTime);
     }
-
+    /// <summary>
+    /// í«è]í‚é~
+    /// </summary>
     public void StopFollow()
     {
         _isFollow = false;
     }
+    /// <summary>
+    /// í«è]äJén
+    /// </summary>
     public void StartFollow()
     {
         if (_followTarget != null)
