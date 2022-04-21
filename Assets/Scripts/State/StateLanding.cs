56dd7eababfeff6f667acbbf9770a4de559269c4
sorude26 <sorude26@gmail.com>
2022-04-21 16:20:45 +0900

    着地ステートを追加した

---
+++ Assets/Scripts/State/StateLanding.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public partial class StateController
+{
+    private class StateLanding : IStateBase
+    {
+        public void OnEnter(StateController controller)
+        {
+            controller._currrentStateType = StateType.Landing;
+        }
+        public void OnFixedUpdate(StateController controller)
+        {
+            controller._moveController.AddGravity();
+            controller._moveController.MoveControl();
+            controller._moveController.MoveDecelerate();
+        }
+        public void OnLeave(StateController controller)
+        {
+        }
+        public void OnUpdate(StateController controller)
+        {
+            controller._stateTimer -= Time.deltaTime;
+            if (controller._stateTimer < 0)
+            {
+                controller.ChangeState(StateType.Idle);
+            }
+        }
+    }
+}
